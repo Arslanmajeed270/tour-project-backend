c27cc00ab53343bf27a9a016ebdff06ede135784
SameeRizwan <49805106+SameeRizwan@users.noreply.github.com>
2021-02-17 20:35:09 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const Cloud = {
+  CLOUD_NAME: "dn3rlyrzy",
+  API_KEY: "581268147237824",
+  API_SECRET: "RXHmNTfU1CnrGzTwBin_8zuMvx8",
+};
+
+const Database = {
+  URL:
+    "mongodb+srv://samee:03458704296sR@cluster0.vbcj6.mongodb.net/Clubs?retryWrites=true&w=majority",
+};
+
+const JWT = {
+  JWT_SECRET: "my-ultra-secure-and-ultra-long-secret",
+  JWT_EXPIRES_IN: "90d",
+  JWT_COOKIE_EXPIRES_IN: "90",
+};
+
+module.exports = JWT;
+module.exports = Cloud;
+module.exports = Database;
